4039128bfd80e934804febea8c5999e2390d7da6
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-01 01:30:49 +0800

    Finished recursive fibonacci

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-function fibs(index) {
-  let fib = [1, 1];
-  index = +index;
+function fibs(count) {
+  let fib = [0, 1];
 
-  if (index === 0) return 0;
+  if (count === 0) return 0;
 
-  for (let i = 2; i <= index; i++) {
+  for (let i = 2; i < count; i++) {
     fib[i] = fib[i - 2] + fib[i - 1];
   }
 
-  if (index >= 0) {
-    return fib[index - 1];
+  if (count > 0) {
+    return fib;
   } else {
-    return "OOPS";
+    return "Error";
   }
 }
+
+function fibsRec(count) {
+  count--;
+  if (count === 0) {
+    return;
+  }
+  if (count < 2) {
+    return [0, 1];
+  }
+  const arr = fibsRec(count);
+  arr.push(arr[count - 1] + arr[count - 2]);
+  return arr;
+}
+
+console.log(fibsRec(8));
